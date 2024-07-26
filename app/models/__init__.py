a58c937427c9d5fa1eca421b83731b7d3c23cfa5
Romeo <pe4unis@gmail.com>
2024-07-26 18:32:36 +0200

    dz5

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from app.models.database import *
 from app.models.questions import *
 from app.models.responses import *
+from app.models.category import *
--- app/models/questions.py
+++ app/models/questions.py
@@ -6,6 +6,8 @@ class Question(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(255), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=True)
+    category = db.relationship('Category', backref=db.backref('questions', lazy=True))
     responses = db.relationship('Response', backref='question', lazy=True)
 
     def __repr__(self):
