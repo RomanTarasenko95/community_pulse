4261930e9dc5a1be84a7164541f2a5df74b24018
Viascheslav Bandylo <viacheslav.bandylo@yandex.ru>
2024-07-26 16:43:46 +0300

    End of Part 2

--- app/routes/questions.py
+++ app/routes/questions.py
@@ -1,22 +1,39 @@
 from flask import Blueprint, request, jsonify, make_response
+from pydantic import ValidationError
 
 from app.models import Question, db
+from app.schemas.questions import QuestionCreate, QuestionResponse
 
 questions_bp = Blueprint('questions', __name__, url_prefix='/questions')
 
 
+# @questions_bp.route('/', methods=['GET'])
+# def get_questions():
+#     questions = Question.query.all()
+#     questions_data = [{'id': q.id, 'text': q.text} for q in questions]
+#     return jsonify(questions_data)
+
+
 @questions_bp.route('/', methods=['GET'])
 def get_questions():
+    """Получение списка всех вопросов."""
     questions = Question.query.all()
-    questions_data = [{'id': q.id, 'text': q.text} for q in questions]
-    return jsonify(questions_data)
+    # Сериализуем объекты SQLAlchemy в Pydantic модели
+    results = [QuestionResponse.from_orm(question).dict() for question in questions]
+    return jsonify(results)
 
 
 @questions_bp.route('/', methods=['POST'])
 def create_question():
     data = request.get_json()
-    if not data or 'text' not in data:
-        return jsonify({'error': 'Missing data'}), 400
+
+    try:
+        question_data = QuestionCreate(**data)
+    except ValidationError as e:
+        return jsonify(e.errors()), 400
+
+    # if not data or 'text' not in data:
+    #     return jsonify({'error': 'Missing data'}), 400
 
     question = Question(text=data['text'])
     db.session.add(question)
